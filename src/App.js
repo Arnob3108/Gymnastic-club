63af8f189a6d8cdb893b5e1e5b0d0ae254a27665
Arnob3108 <sajedurrahmanarnob@gmail.com>
2022-09-29 10:31:31 +0600

    header added with logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import logo from "./logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
 }
 
 export default App;
